676cbd461dc640d53ec4c51f47a8d511d19838f1
user1 <dominic.larkin@usma.edu>
2016-01-29 16:57:54 -0500

    publishing goal still not working

--- src/pioneer_imu_test.py
+++ src/pioneer_imu_test.py
@@ -3,12 +3,15 @@
 import roslib
 import rospy
 import sys
+import time
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from move_base_msgs.msg import MoveBaseActionGoal
 
 print len(sys.argv)
-pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=5)
+#pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=5)
+pub = rospy.Publisher('/move_base/goal', MoveBaseActionGoal, queue_size=5)
 if len(sys.argv) == 3:
     delta_x = sys.argv[1]
     delta_y = sys.argv[2]
@@ -22,17 +25,28 @@ else:
 global sub_once
 
 def odom_callback(data):
-    goal_pose=PoseStamped()
-    goal_pose.header.frame_id = 'map'
+    #goal_pose=PoseStamped()
+    #goal_pose.header.frame_id = 'map'
+    #goal_pose.header.stamp = data.header.stamp
+    #goal_pose.pose.position.x = data.pose.pose.position.x + delta_x
+    #goal_pose.pose.position.y = data.pose.pose.position.y + delta_y    
+    #goal_pose.pose.orientation = data.pose.pose.orientation
+
+    goal_pose=MoveBaseActionGoal()
     goal_pose.header.stamp = data.header.stamp
-    goal_pose.pose.position.x = data.pose.pose.position.x + delta_x
-    goal_pose.pose.position.y = data.pose.pose.position.y + delta_y    
-    goal_pose.pose.orientation = data.pose.pose.orientation
+    goal_pose.header.frame_id = 'map'
+    goal_pose.goal_id.stamp = data.header.stamp
+    goal_pose.goal_id.id = "custom"    
+    goal_pose.goal.target_pose.header = goal_pose.header
+    goal_pose.goal.target_pose.pose.position.x = data.pose.pose.position.x + delta_x
+    goal_pose.goal.target_pose.pose.position.y = data.pose.pose.position.y + delta_y    
+    goal_pose.goal.target_pose.pose.orientation = data.pose.pose.orientation
     pub.publish(goal_pose)
-    str = "\nMoving from ({0},{1})  to goal of ({2},{3})".format(data.pose.pose.position.x,data.pose.pose.position.y,goal_pose.pose.position.x, goal_pose.pose.position.y )
+    str = "\nMoving from ({0},{1})  to goal of ({2},{3})".format(data.pose.pose.position.x,data.pose.pose.position.y,goal_pose.goal.target_pose.pose.position.x, goal_pose.goal.target_pose.pose.position.y )
     rospy.loginfo(str)
-    #sub_once.unregister()
-    #rospy.signal_shutdown("Published a goal")
+    time.sleep(15)
+    sub_once.unregister()
+    rospy.signal_shutdown("Published a goal")
 
 if __name__ == '__main__':
     rospy.init_node('simple_goal_Py', anonymous=True)
